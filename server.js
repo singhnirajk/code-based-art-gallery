435aacc1db4e27d3e4f85983462cf8f60c403fac
Nirajkumar Yogendra Singh <66298582+singhnirajk@users.noreply.github.com>
2024-03-23 15:13:22 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Catch-all route to serve index.html
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'code-based-art-gallery', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
